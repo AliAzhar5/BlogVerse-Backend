b48b9b6873dba8e64a20d5828d5a1a0c9b440c6e
AliAzhar5 <aalliiazhar@gmail.com>
2024-12-26 03:13:23 +0500

    updated build_files.sh

--- build_files.sh
+++ build_files.sh
@@ -1,6 +1,3 @@
-python3.12 -m pip install -r requirements.txt
+pip install -r requirements.txt
 
-python3.12 manage.py makemigrations --noinput
-python3.12 manage.py migrate --noinput
-
-python3.12 manage.py collectstatic --noinput
+python3.9 manage.py collectstatic
